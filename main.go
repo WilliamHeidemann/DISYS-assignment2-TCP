380c73edafaae325abe9144fbfd0f45284a4c376
William Skou Heidemann <wihe@itu.dk>
2022-09-21 20:28:43 +0200

    updated how string is split and added fmt print lines

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 type server struct {
 	tcpStream chan clientPacket
 }
@@ -9,8 +14,10 @@ type client struct {
 }
 
 type clientPacket struct {
-	message string
-	client  client
+	message      string
+	client       client
+	index        int
+	totalPackets int
 }
 
 func main() {
@@ -18,19 +25,22 @@ func main() {
 		make(chan clientPacket),
 	}
 
-	go server.listenForConnections()
+	go server.listenForPackets()
 
 	client := &client{
 		make(chan string),
 	}
 
 	go client.connectToServer(server)
+	time.Sleep(time.Minute)
 }
 
 func (client *client) connectToServer(server *server) {
 	synPacket := clientPacket{
 		"SYN",
 		*client,
+		1,
+		1,
 	}
 	server.tcpStream <- synPacket
 	response := <-client.tcpStream
@@ -39,25 +49,76 @@ func (client *client) connectToServer(server *server) {
 		synAckAckPacket := clientPacket{
 			"SYN ACK ACK",
 			*client,
+			1,
+			1,
 		}
 		server.tcpStream <- synAckAckPacket
+		message := "Hello World!"
+		var messagePackets []clientPacket
+		messagePackets = createMessagePackets(message, client)
+		fmt.Println("Message packets created!")
+		for i, packet := range messagePackets {
+			fmt.Printf("Sending packet #%d\n", i)
+			server.tcpStream <- packet
+			<-client.tcpStream // Packet was received
+		}
 	}
 }
 
-func (server *server) listenForConnections() {
+func createMessagePackets(message string, client *client) []clientPacket {
+	packetSize := 5
+	length := len(message) / packetSize
+	if len(message)%packetSize != 0 {
+		length += 1
+	}
+	var packets []clientPacket
+	packets = make([]clientPacket, length)
+	for i := 0; i < length; i++ {
+		var sliceOfMessage string
+		if i*packetSize+packetSize < len(message) {
+			sliceOfMessage = message[i*packetSize : i*packetSize+packetSize]
+		} else {
+			sliceOfMessage = message[i*packetSize:]
+		}
+		packet := clientPacket{
+			sliceOfMessage,
+			*client,
+			i,
+			length,
+		}
+		packets = append(packets, packet)
+	}
+
+	return packets
+}
+
+func (server *server) listenForPackets() {
 	for {
 		packet := <-server.tcpStream
 		switch packet.message {
 		case "SYN":
 			packet.client.tcpStream <- "SYN ACK"
 		case "SYN ACK ACK":
+			fmt.Println("Connection between server and client established!")
 			var packets []clientPacket
 			packets = make([]clientPacket, 10)
 			for {
 				packet = <-server.tcpStream
+				fmt.Printf("Packet #%d received\n", packet.index)
 				// validation
-				append(packets, packet)
+				packets = append(packets, packet)
+				packet.client.tcpStream <- "Packet received!"
+				fmt.Println("Ack packet returned")
+				if packet.index == packet.totalPackets-1 {
+					fmt.Println("All packets were received!")
+					break
+				}
 			}
+			var originalMessage string
+			for _, packet := range packets {
+				originalMessage += packet.message
+			}
+			fmt.Println("Received message from client: \"" + originalMessage + "\"")
 		}
 	}
 }
