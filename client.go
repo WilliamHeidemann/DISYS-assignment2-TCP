315b77dd6885b08dca26df7e3291dbd424c66b43
William Skou Heidemann <wihe@itu.dk>
2022-09-27 15:53:42 +0200

    sent message through client method parameter instead

--- client.go
+++ client.go
@@ -18,7 +18,7 @@ type clientPacket struct {
 	server       server
 }
 
-func (client *client) connectToServer(server *server) {
+func (client *client) sendMessageTCP(server *server, message string) {
 	synPacket := clientPacket{
 		"SYN",
 		*client,
@@ -40,7 +40,6 @@ func (client *client) connectToServer(server *server) {
 		}
 		forward(synAckAckPacket)
 
-		message := "We're soarin', flyin'\nThere's not a star in heaven\nThat we can't reach\nIf we're trying\nSo we're breaking free\nYou know the world can see us\nIn a way that's different than who we are\nCreating space between us\n'Til we're separate hearts\nBut your faith it gives me strength\nStrength to believe\nWe're breakin' free\n[Gabriella:]\nWe're soarin'\n[Troy:]\nFlyin'\n[Both:]\nThere's not a star in heaven\nThat we can't reach\n[Troy:]\nIf we're trying\n[Both:]\nYeah, we're breaking free\n[Troy:]\nOh, we're breakin' free\n[Gabriella:]\nOhhhh\n[Troy:]\nCan you feel it building\nLike a wave the ocean just can't control\n[Gabriella:]\nConnected by a feeling\nOhhh, in our very souls\n[Troy:]\nVery souls, ohhh\n[Both:]\nRising 'til it lifts us up\nSo everyone can see us\nWe're breakin' free\n[Gabriella:]\nWe're soarin'\n[Troy:]\nFlyin'\n[Both:]\nThere's not a star in heaven\nThat we can't reach\n[Troy:]\nIf we're trying\nYeah we're breaking free\n[Gabriella:]\nOhhhh runnin'\n[Troy:]\nClimbin'\nTo get to that place\n[Both:]\nTo be all that we can be\n[Troy:]\nNow's the time\n[Both:]\nSo we're breaking free\n[Troy:]\nWe're breaking free\n[Gabriella:]\nOhhh, yeah\nMore than hope\nMore than faith\n[Gabriella:]\nThis is true\nThis is fate\nAnd together\nWe see it comin'\n[Troy:]\nMore than you\nMore than me\nNot a want, but a need\n[Both:]\nBoth of us breakin' free\nSoarin'\n[Troy:]\nFlyin'\n[Both:]\nThere's not a star in heaven\nThat we can't reach\nIf we're trying\n[Troy:] Yeah we're breaking free\n[Gabriella:]\nBreaking free\nWere runnin'\n[Troy:]\nOhhhh, climbin'\n[Both:]\nTo get to the place\nTo be all that we can be\nNow's the time\n[Troy:] Now's the time\n[Gabriella:] So we're breaking free\n[Troy:] Ohhh, we're breaking free\n[Gabriella:] Ohhhh"
 		var messagePackets []clientPacket
 		messagePackets = client.marshalMessage(message, server)
 		fmt.Println("Message packets created!")
